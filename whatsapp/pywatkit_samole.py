bc30cd399313c28b4f4a5951cc9110560af07b5e
Kharshitha Bhuvani E <69036407+Kharshitha16@users.noreply.github.com>
2022-11-01 15:58:47 +0530

    Add files via upload

---
+++ whatsapp/pywatkit_samole.py
@@ -0,0 +1,15 @@
+import pywhatkit as pwk
+
+# using Exception Handling to avoid unexpected errors
+
+# sending message in Whatsapp in India so using Indian dial code (+91)
+pwk.sendwhatmsg("+919150413708", "https://thumbs.dreamstime.com/b/golden-retriever-dog-21668976.jpg",17,40)
+#pwk.sendwhats_image("+919344580944", "C:\\Users\\KharshithaBhuvaniEla\\Desktop\\chatbotvenv for doc\\venv2\\Lib\\sdf.jpg")
+#pwk.sendwhatmsg_to_group("Oops lab", "Hey Guys! How's everybody?",15,24)
+#pwk.playonyt("https://www.youtube.com/watch?v=r1aDa-kWROk")
+# pwk.search("GeeksforGeeks")
+
+print("Message Sent!")  # Prints success message in console
+
+
+    # error message
